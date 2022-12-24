32bb8db8c1cf017f6c579f9b1109eb2b66004ed6
Adrian <adriansurdu33@gmail.com>
2022-12-24 17:23:29 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import TopButtons from "./components/TopButtons";
+import SearchBar from "./components/SearchBar"
+import TimeAndLocation from "./components/TimeAndLocation";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div
+            className="mx-auto max-w-screen-md mt-4 py-5 px-35 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+            <TopButtons/>
+            <SearchBar/>
+            <TimeAndLocation/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TimeAndLocation = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-center my-6">
+                <p className="text-white text-xl font-extra-light">
+                    Saturday, 24 December | Local time: 17:21
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default TimeAndLocation;
